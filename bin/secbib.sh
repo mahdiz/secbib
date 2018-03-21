c8d1c790e6156866fe6b5013a1206025aa8e3dc6
Mahdi Zamani <mzamani@visa.com>
2018-03-21 15:26:28 -0700

    page number fix

--- bin/secbib.sh
+++ bin/secbib.sh
@@ -3,4 +3,4 @@
 ## Sort the file
 cp ../secbib.bib secbib.bak
 bibtool -s -i ../secbib.bib -o ../secbib.bib
-bibtool -r ../style.rsc -i ../secbib.bib -o ../secbib.bib
+bibtool -r style.rsc -i ../secbib.bib -o ../secbib.bib
